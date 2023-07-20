f8fc28b45646e9b79cf1d6fb7c704ac9d5d74cb2
BrendaMonis <brenda.monis.moreno@usp.br>
2023-07-20 15:09:48 -0300

    testando

---
+++ git.R
@@ -0,0 +1,11 @@
+gitcreds::gitcreds_set()
+
+usethis::use_git_remote()
+rlang::last_trace()
+
+usethis::use_git_remote(
+  "origin",
+  url = "https://github.com/BrendaMonis/Shiny_app_BeP.git",
+  overwrite = TRUE
+)
+
